edc731d17495c61d0fd71c8b3e57f1a0a7b5df11
kelly r maass <kmaass77627@gmail.com>
2020-09-25 05:06:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("HELLO WORLD")
 
+  fetch("http://localhost:3000/toys")
+  .then(function(response) {
+    response.json
+})
+  .then(function(object){
+    console.log(object)
+  })
+
 });
